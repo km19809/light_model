ea382ff5263b2f635c178469f0aa4b07fe838d47
Minsoo Kim <gkm19809@gmail.com>
2020-05-22 16:16:58 +0900

    added root handler

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import torch
 from torchtext import data
 from generation import inference, tokenizer1
@@ -61,6 +61,9 @@ def soft():
 # def rough():
 #     return inference(device, max_len, TEXT, LABEL, rough_model, ), 200
 
+@app.route('/', methods=['GET'])
+def main_page():
+    return send_from_directory('static','main.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
